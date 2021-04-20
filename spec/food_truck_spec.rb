9e59978b8a87524fc4e1ce551679fcc91af12da1
NoahZinter <noah.zinter@gmail.com>
2021-04-20 13:12:05 -0600

    Test food truck exists

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,2 +1,10 @@
 require './lib/item'
 require './lib/food_truck'
+
+describe FoodTruck do
+  it 'exists' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    expect(food_truck).is_a? FoodTruck
+  end
+end
