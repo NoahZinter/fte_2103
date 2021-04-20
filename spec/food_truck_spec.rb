48df4df695319e975b3aa17da763b83f0ded6e1d
NoahZinter <noah.zinter@gmail.com>
2021-04-20 14:01:28 -0600

    Refactor food truck spec for use of item objects

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -23,33 +23,38 @@ describe FoodTruck do
   describe '#check_stock' do
     it 'returns zero as a default' do
       food_truck = FoodTruck.new("Rocky Mountain Pies")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
 
-      expect(food_truck.check_stock("Borscht")).to eq 0
+      expect(food_truck.check_stock(item1)).to eq 0
     end
 
     it 'returns the integer quantity of item at key' do
       food_truck = FoodTruck.new("Rocky Mountain Pies")
-      food_truck.stock("Kebab", 20)
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      food_truck.stock(item1, 20)
 
-      expect(food_truck.check_stock("Kebab")).to eq 20
+      expect(food_truck.check_stock(item1)).to eq 20
     end
   end
 
   describe '#stock' do
     it 'adds item and quantity as k/v pair in @inventory' do
       food_truck = FoodTruck.new("Rocky Mountain Pies")
-      food_truck.stock("Kofta", 50)
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      food_truck.stock(item1, 50)
 
-      expect(food_truck.check_stock("Kofta")).to eq 50
+      expect(food_truck.check_stock(item1)).to eq 50
     end
 
     it 'can add multiple item/quantity pairs to inventory' do
       food_truck = FoodTruck.new("Rocky Mountain Pies")
-      food_truck.stock("Kofta", 50)
-      food_truck.stock("Gumbo", 18)
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      food_truck.stock(item1, 50)
+      food_truck.stock(item2, 18)
       expected = {
-        "Kofta" => 50,
-        "Gumbo" => 18
+        item1 => 50,
+        item2 => 18
       }
 
       expect(food_truck.inventory).to eq expected
